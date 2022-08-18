77fd677629eb0794c8396ee7bd18139465ea9c5a
Rumoon <zzp54333zzm@gmail.com>
2022-08-18 18:10:35 +0800

    et课程-修复代码错误

--- Server/Hotfix/Demo/Account/handler/C2G_LoginGameGateHandler.cs
+++ Server/Hotfix/Demo/Account/handler/C2G_LoginGameGateHandler.cs
@@ -3,6 +3,7 @@ using System.Security;
 
 namespace ET
 {
+    [FriendClass(typeof(SessionPlayerComponent))]
     public class C2G_LoginGameGateHandler: AMRpcHandler<C2G_LoginGameGate, G2C_LoginGameGate>
     {
         protected override async ETTask Run(Session session, C2G_LoginGameGate request, G2C_LoginGameGate response, Action reply)
